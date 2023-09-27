151a118603adf22d942750f9f6235673e66c0c8f
Diego Cruz <dcruz@indocal.gob.do>
2023-09-26 22:45:11 -0400

    mend

---
+++ features/CertificatePDF/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as background } from "./background.png";
+export { default as logo } from "./logo.png";
+export { default as stamp } from "./stamp.png";
+export { default as signature } from "./example.png";
